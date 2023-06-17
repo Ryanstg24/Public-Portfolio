f4500452b8a6d4c44124d6bd55df9db938280378
Ryan S <55468434+Ryanstg24@users.noreply.github.com>
2023-06-17 18:38:26 -0400

    Uploaded working weather app

---
+++ Personal Projects/Weather App/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8e2d509944mshbed96c4c82b207dp1e136ejsn5f433e8dac8b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b3ef9469510483a9745f34f6912f5230";
